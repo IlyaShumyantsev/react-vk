c9c3ce58d8e3be23083000cdc8e99c0ea1f6596e
IlyaShumyantsev <ilya.shumyantsev@gmail.com>
2021-02-23 20:49:53 +0300

    removed unnecessary things

--- src/components/Modals/CommentsModal.js
+++ src/components/Modals/CommentsModal.js
@@ -10,70 +10,62 @@ const CommetnsModal = ({ modal, handleCommentsModal }) => {
   };
 
   return (
-    <div>
-      <Modal isOpen={modal.isOpen} toggle={onToggle}>
-        <ModalHeader toggle={onToggle}>Комментарии к фото</ModalHeader>
-        {modal?.comments?.map((comment, index) => (
-          <ModalBody key={index}>
-            <div class="media border p-3">
-              <img
-                src={comment.user[0].photo_50}
-                alt="John Doe"
-                class="mr-3 mt-3 rounded-circle"
-                style={style.image}
-              />
-              <div className="media-body">
-                <h4>
-                  {comment.user[0].first_name}&nbsp;
-                  <small>
-                    <i className="text-primary">{new Date(comment.date * 1000).toLocaleString()}</i>
-                  </small>
-                </h4>
-                <p>
-                  {comment?.text}
-                  <br></br>
-                  {comment.attachments &&
-                    comment.attachments.map((attachment, index) => (
-                      <div>
-                        {attachment.photo && (
-                          <img
-                            src={attachment.photo?.sizes[attachment.photo?.sizes.length - 1].url}
-                            alt=""
-                            height="200px"
-                            key={index}
-                            className="m-1"
-                          />
-                        )}
-                        {attachment.sticker && (
-                          <img
-                            src={
-                              attachment.sticker?.images[attachment.sticker?.images.length - 1].url
-                            }
-                            alt=""
-                            height="200px"
-                            key={index}
-                            className="m-1"
-                          />
-                        )}
-                        {attachment.audio && (
-                          <video controls name="media">
-                            <source type="audio/mpeg" src={attachment.audio.url} />
-                          </video>
-                        )}
-                      </div>
-                    ))}
-                </p>
-              </div>
+    <Modal isOpen={modal.isOpen} toggle={onToggle} width="400px">
+      <ModalHeader toggle={onToggle}>Комментарии к фото</ModalHeader>
+      {modal?.comments?.map((comment, index) => (
+        <ModalBody key={index}>
+          <div className="media border p-3">
+            <img
+              src={comment.user[0].photo_50}
+              alt="John Doe"
+              className="mr-3 mt-3 rounded-circle"
+              style={style.image}
+            />
+            <div className="media-body">
+              <h4>
+                {comment.user[0].first_name}&nbsp;
+                <small>
+                  <i className="text-primary">{new Date(comment.date * 1000).toLocaleString()}</i>
+                </small>
+              </h4>
+              {comment?.text}
+              <br></br>
+              {comment.attachments &&
+                comment.attachments.map((attachment, index) => (
+                  <div key={index}>
+                    {attachment.photo && (
+                      <img
+                        src={attachment.photo?.sizes[attachment.photo?.sizes.length - 1].url}
+                        alt=""
+                        height="190px"
+                        className="m-1"
+                      />
+                    )}
+                    {attachment.sticker && (
+                      <img
+                        src={attachment.sticker?.images[attachment.sticker?.images.length - 1].url}
+                        alt=""
+                        height="200px"
+                        className="m-1"
+                      />
+                    )}
+                    {attachment.audio && (
+                      <video controls name="media">
+                        <source type="audio/mpeg" src={attachment.audio.url} />
+                      </video>
+                    )}
+                  </div>
+                ))}
             </div>
-          </ModalBody>
-        ))}
-        <ModalFooter>
-          <Button color="danger" onClick={onToggle}>
-            Закрыть
-          </Button>
-        </ModalFooter>
-      </Modal>
-    </div>
+          </div>
+        </ModalBody>
+      ))}
+      <ModalFooter>
+        <Button color="danger" onClick={onToggle}>
+          Закрыть
+        </Button>
+      </ModalFooter>
+    </Modal>
   );
 };
 
